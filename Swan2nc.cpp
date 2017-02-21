5f77b75e52bad13c4aa4b9ead54241185155b763
Cyprien Bosserelle <cyprien.bosserelle@gmail.com>
2017-02-21 14:18:45 +1200

    First Commit

---
+++ Swan2nc.cpp
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <cmath>
+#include <fstream>
+#include <netcdf.h>
+#include <algorithm>
+
+
+#include <math.h>
+
+
+char infile[256];
+char ncoutfile[256];
+char outvarnameA[256];
+char outvarnameB[256];
+float * xx,*yy,*tt;
+short *zz,*vv,*uu;
+float xo,yo,to;
+float dx,dy,dt;
+int nx,ny,nt;
+int nvar;
+
+//void creatncfile(char outfile[], int nx,int ny,float *xval, float *yval,float totaltime,short *Hs);
+void creatncfile(char outfile[],char varname[], int nx,int ny,float *xval, float *yval,float totaltime,short *Hs);
+void creatncfile2(char outfile[],char varnameA[],char varnameB[], int nx,int ny,float *xval, float *yval,float totaltime,short *uu,short *vv);
+//void writestep2nc(char outfile[], int nx,int ny,float totaltime,short *Hs);
+void writestep2nc(char outfile[],char varname[], int nx,int ny,float totaltime,short *Hs);
+void writestep2nc2(char outfile[],char varnameA[], char varnameB[],int nx,int ny,float totaltime,short *uu,short *vv);
+
+int main(int argc, char **argv)
+{
+	//Read operational file
+	char opfile[]="Swan2nc.dat";
+	FILE * fop;
+    fop=fopen(opfile,"r");
+	
+	fscanf(fop,"%*s %s\t%*s",&infile);
+	fscanf(fop,"%d\t%*s",&nvar);
+	fscanf(fop,"%f,%f\t%*s",&xo,&yo);
+	fscanf(fop,"%f,%f\t%*s",&dx,&dy);
+	fscanf(fop,"%d,%d\t%*s",&nx,&ny);
+	fscanf(fop,"%f,%f,%d\t%*s",&to,&dt,&nt);
+	fscanf(fop,"%s\t%*s",&ncoutfile);
+	fscanf(fop,"%s\t%*s",&outvarnameA);
+	fscanf(fop,"%s\t%*s",&outvarnameB);
+	fclose(fop);
+
+	printf("Infile:%s\n nvar=%d\n xo=%f;  dx=%f;  nx=%d\n yo=%f;  dy=%f;  ny=%d\n to=%f;  dt=%f;  nt=%d\n outfile:%s\n",infile,nvar,xo,dx,nx,yo,dy,ny,to,dt,nt,ncoutfile);
+
+	//Allocate memory for read variable
+	xx=(float *)malloc(nx*sizeof(float ));
+	yy=(float *)malloc(ny*sizeof(float ));
+	tt=(float *)malloc(nt*sizeof(float ));
+	zz= (short *)malloc(nx*ny*sizeof(short));
+	if (nvar>1)
+	{
+		uu=(short *)malloc(nx*ny*sizeof(short));
+	}
+	
+
+
+	//create xx and yy array
+	for (int i=0; i<nx; i++)
+	{
+		xx[i]=xo+i*dx;
+	}
+
+	for (int j=0; j<ny; j++)
+	{
+		yy[j]=yo+j*dy;
+	}
+
+	
+
+
+	//open the output file and read the first step nt should at least be size 1
+	FILE * fin;
+    fin=fopen(infile,"r");
+	float Dum;
+
+	for (int i=0; i<nx; i++)
+	{
+		for (int j=0; j<ny; j++)
+		{
+			fscanf(fin,"%f",&Dum);
+			Dum=Dum*100.0f;
+			zz[j+ny*i]=(short)(Dum);
+		}
+	}
+
+
+
+
+
+	//Prepare netcdf file
+	if(nvar<2)
+	{
+	creatncfile(ncoutfile,outvarnameA,nx,ny,xx,yy,to,zz);
+	}
+	else
+	{
+		for (int i=0; i<nx; i++)
+		{
+			for (int j=0; j<ny; j++)
+			{
+				fscanf(fin,"%f",&Dum);
+				Dum=Dum*100.0f;
+				uu[j+ny*i]=(short)(Dum);
+			}
+		}
+		creatncfile2(ncoutfile,outvarnameA,outvarnameB,nx,ny,xx,yy,to,zz,uu);
+	}
+
+	for (int nn=1; nn<nt; nn++)
+	{
+		for (int i=0; i<nx; i++)
+		{
+			for (int j=0; j<ny; j++)
+			{
+				fscanf(fin,"%f",&Dum);
+				Dum=Dum*100.0f;
+				zz[j+ny*i]=(short)(Dum);
+			}
+		}
+	
+
+	if(nvar<2)
+	{
+		writestep2nc(ncoutfile,outvarnameA,nx,ny,to+nn*dt,zz);
+	}
+	else
+	{
+		for (int i=0; i<nx; i++)
+		{
+			for (int j=0; j<ny; j++)
+			{
+				fscanf(fin,"%f",&Dum);
+				Dum=Dum*100.0f;
+				uu[j+ny*i]=(short)(Dum);
+			}
+		}
+		writestep2nc2(ncoutfile,outvarnameA,outvarnameB,nx,ny,to+nn*dt,zz,uu);
+	}
+
+	}
+
+	fclose(fin);
+
+
+}
---
+++ writexyz.cpp
@@ -0,0 +1,306 @@
+//////////////////////////////////////////////////////////////////////////////////
+//SWAN2nc                                                                    //
+//Copyright (C) 2013 Bosserelle                                                 //
+//                                                                              //
+//This program is free software: you can redistribute it and/or modify          //
+//it under the terms of the GNU General Public License as published by          //
+//the Free Software Foundation.                                                 //
+//                                                                              //
+//This program is distributed in the hope that it will be useful,               //
+//but WITHOUT ANY WARRANTY; without even the implied warranty of                //    
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 //
+//GNU General Public License for more details.                                  //
+//                                                                              //
+//You should have received a copy of the GNU General Public License             //
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.         //
+//////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netcdf.h>
+using namespace std;
+//(xp,yp,zp,tp,xl,yl, npart,fileoutn)
+void writexyz(float * x, float * y, float * z,float *t,float *xl,float *yl,  int npart,char outfile[])
+{
+	FILE * ofile;
+	ofile= fopen (outfile,"w");
+
+	for (int i=1; i<=npart; i++)
+	{
+		if(t[i-1]>=0.0f)// Do not output particle that haven't been released yet
+		{
+			fprintf(ofile,"%f\t%f\t%f\t%f\t%f\t%f\n",x[i-1],y[i-1],z[i-1],t[i-1],xl[i-1],yl[i-1]);
+		}
+	}
+	fclose (ofile);
+}
+void creatncfile(char outfile[],char varname[], int nx,int ny,float *xval, float *yval,float totaltime,short *Hs)
+{               
+	int status;
+   	int ncid,xx_dim,yy_dim,time_dim,p_dim;
+	size_t nxx,nyy,nnpart;
+	int  var_dimids[3], var2_dimids[2];
+	
+	int Nin_id,cNin_id,cTin_id,time_id,xx_id,yy_id;
+	
+	nxx=nx;
+	nyy=ny;
+	//nnpart=npart;
+
+	static size_t start[] = {0, 0, 0}; // start at first value 
+    	static size_t count[] = {1, ny, nx};
+		//static size_t pstart[] = {0, 0}; // start at first value 
+   // 	static size_t pcount[] = {1, npart};
+	static size_t tst[]={0};
+	static size_t xstart[] = {0}; // start at first value 
+    	static size_t xcount[] = {nx};
+	
+	static size_t ystart[] = {0}; // start at first value 
+    	static size_t ycount[] = {ny};
+	const float hund=0.01;
+
+
+
+
+	//create the netcdf dataset
+	status = nc_create(outfile, NC_NOCLOBBER, &ncid);
+	
+	//Define dimensions: Name and length
+	
+	status = nc_def_dim(ncid, "x", nxx, &xx_dim);
+	status = nc_def_dim(ncid, "y", nyy, &yy_dim);
+	//status = nc_def_dim(ncid, "npart",nnpart,&p_dim);
+	status = nc_def_dim(ncid, "time", NC_UNLIMITED, &time_dim);
+	int tdim[]={time_dim};
+	int xdim[]={xx_dim};
+	int ydim[]={yy_dim};
+	//int pdim[2];
+	//pdim[0]=time_dim;
+	//pdim[1]=p_dim;
+	//define variables: Name, Type,...
+	var_dimids[0] = time_dim;
+    var_dimids[1] = yy_dim;
+    var_dimids[2] = xx_dim;
+	
+	var2_dimids[0] = yy_dim;
+    var2_dimids[1] = xx_dim;
+
+	
+   	status = nc_def_var (ncid, "time", NC_FLOAT,1,tdim, &time_id);
+	status = nc_def_var (ncid, "x", NC_FLOAT,1,xdim, &xx_id);
+	status = nc_def_var (ncid, "y", NC_FLOAT,1,ydim, &yy_id);
+	//status = nc_def_var (ncid, "xxp", NC_FLOAT,2,pdim, &xxp_id);
+	//status = nc_def_var (ncid, "yyp", NC_FLOAT,2,pdim, &yyp_id);
+	
+   	status = nc_def_var (ncid, varname, NC_SHORT, 3, var_dimids, &Nin_id);
+
+	status = nc_put_att_float (ncid, Nin_id, "scale_factor", NC_FLOAT,1, &hund);
+    
+	//put attriute: assign attibute values
+	//nc_put_att
+
+	//End definitions: leave define mode
+	status = nc_enddef(ncid);  
+
+	//Provide values for variables
+	status = nc_put_var1_float(ncid, time_id,tst,&totaltime);
+	status = nc_put_vara_float(ncid, xx_id,xstart,xcount, xval);
+	status = nc_put_vara_float(ncid, yy_id,ystart,ycount, yval);
+	//status = nc_put_vara_float(ncid, xxp_id,pstart,pcount, xxp);
+	//status = nc_put_vara_float(ncid, yyp_id,pstart,pcount, yyp);
+		
+	
+	status = nc_put_vara_short(ncid, Nin_id, start, count, Hs);
+
+	
+
+	//close and save new file
+	status = nc_close(ncid);  
+}
+
+void creatncfile2(char outfile[],char varnameA[],char varnameB[], int nx,int ny,float *xval, float *yval,float totaltime,short *uu,short *vv)
+{               
+	int status;
+   	int ncid,xx_dim,yy_dim,time_dim,p_dim;
+	size_t nxx,nyy,nnpart;
+	int  var_dimids[3], var2_dimids[2];
+	
+	int Nin_id,cNin_id,cTin_id,time_id,xx_id,yy_id;
+	
+	nxx=nx;
+	nyy=ny;
+	//nnpart=npart;
+
+	static size_t start[] = {0, 0, 0}; // start at first value 
+    	static size_t count[] = {1, ny, nx};
+		//static size_t pstart[] = {0, 0}; // start at first value 
+   // 	static size_t pcount[] = {1, npart};
+	static size_t tst[]={0};
+	static size_t xstart[] = {0}; // start at first value 
+    	static size_t xcount[] = {nx};
+	
+	static size_t ystart[] = {0}; // start at first value 
+    	static size_t ycount[] = {ny};
+	const float hund=0.01;
+
+
+
+
+	//create the netcdf dataset
+	status = nc_create(outfile, NC_NOCLOBBER, &ncid);
+	
+	//Define dimensions: Name and length
+	
+	status = nc_def_dim(ncid, "x", nxx, &xx_dim);
+	status = nc_def_dim(ncid, "y", nyy, &yy_dim);
+	//status = nc_def_dim(ncid, "npart",nnpart,&p_dim);
+	status = nc_def_dim(ncid, "time", NC_UNLIMITED, &time_dim);
+	int tdim[]={time_dim};
+	int xdim[]={xx_dim};
+	int ydim[]={yy_dim};
+	//int pdim[2];
+	//pdim[0]=time_dim;
+	//pdim[1]=p_dim;
+	//define variables: Name, Type,...
+	var_dimids[0] = time_dim;
+    var_dimids[1] = yy_dim;
+    var_dimids[2] = xx_dim;
+	
+	var2_dimids[0] = yy_dim;
+    var2_dimids[1] = xx_dim;
+
+	
+   	status = nc_def_var (ncid, "time", NC_FLOAT,1,tdim, &time_id);
+	status = nc_def_var (ncid, "x", NC_FLOAT,1,xdim, &xx_id);
+	status = nc_def_var (ncid, "y", NC_FLOAT,1,ydim, &yy_id);
+	//status = nc_def_var (ncid, "xxp", NC_FLOAT,2,pdim, &xxp_id);
+	//status = nc_def_var (ncid, "yyp", NC_FLOAT,2,pdim, &yyp_id);
+	
+   	status = nc_def_var (ncid, varnameA, NC_SHORT, 3, var_dimids, &Nin_id);
+	status = nc_def_var (ncid, varnameB, NC_SHORT, 3, var_dimids, &cNin_id);
+
+	status = nc_put_att_float (ncid, Nin_id, "scale_factor", NC_FLOAT,1, &hund);
+    
+	//put attriute: assign attibute values
+	//nc_put_att
+
+	//End definitions: leave define mode
+	status = nc_enddef(ncid);  
+
+	//Provide values for variables
+	status = nc_put_var1_float(ncid, time_id,tst,&totaltime);
+	status = nc_put_vara_float(ncid, xx_id,xstart,xcount, xval);
+	status = nc_put_vara_float(ncid, yy_id,ystart,ycount, yval);
+	//status = nc_put_vara_float(ncid, xxp_id,pstart,pcount, xxp);
+	//status = nc_put_vara_float(ncid, yyp_id,pstart,pcount, yyp);
+		
+	
+	status = nc_put_vara_short(ncid, Nin_id, start, count, uu);
+	status = nc_put_vara_short(ncid, cNin_id, start, count, vv);
+	
+
+	//close and save new file
+	status = nc_close(ncid);  
+}
+
+void writestep2nc(char outfile[],char varname[], int nx,int ny,float totaltime,short *Hs)
+{
+	int status;
+   	int ncid,time_dim,recid;
+	size_t nxx,nyy;
+	int Nincel_id,cNincel_id,cTincel_id,time_id;
+	static size_t start[] = {0, 0, 0}; // start at first value 
+    	static size_t count[] = {1, ny, nx};
+ 	//static size_t pstart[] = {0, 0}; // start at first value 
+    //	static size_t pcount[] = {1, npart};
+	static size_t tst[]={0};
+
+	nxx=nx;
+	nyy=ny;
+
+	
+	static size_t nrec;
+	status = nc_open(outfile, NC_WRITE, &ncid);
+	
+	//read id from time dimension
+	status = nc_inq_unlimdim(ncid, &recid);
+	status = nc_inq_dimlen  (ncid, recid, &nrec);
+	printf("nrec=%d\n",nrec);
+
+	//read file for variable ids
+	status = nc_inq_varid(ncid, "time", &time_id);
+	
+	status = nc_inq_varid(ncid, varname, &Nincel_id);
+
+	
+	
+	
+	start[0] = nrec;
+	//pstart[0] = nrec;    
+	tst[0]=nrec;
+
+	//Provide values for variables
+	status = nc_put_var1_float(ncid, time_id,tst,&totaltime);
+	
+	status = nc_put_vara_short(ncid, Nincel_id, start, count, Hs);
+
+	
+	
+
+	//close and save
+	status = nc_close(ncid);
+
+
+}
+void writestep2nc2(char outfile[],char varnameA[], char varnameB[],int nx,int ny,float totaltime,short *uu,short *vv)
+{
+	int status;
+   	int ncid,time_dim,recid;
+	size_t nxx,nyy;
+	int Nincel_id,cNincel_id,cTincel_id,time_id;
+	static size_t start[] = {0, 0, 0}; // start at first value 
+    	static size_t count[] = {1, ny, nx};
+ 	//static size_t pstart[] = {0, 0}; // start at first value 
+    //	static size_t pcount[] = {1, npart};
+	static size_t tst[]={0};
+
+	nxx=nx;
+	nyy=ny;
+
+	
+	static size_t nrec;
+	status = nc_open(outfile, NC_WRITE, &ncid);
+	
+	//read id from time dimension
+	status = nc_inq_unlimdim(ncid, &recid);
+	status = nc_inq_dimlen  (ncid, recid, &nrec);
+	printf("nrec=%d\n",nrec);
+
+	//read file for variable ids
+	status = nc_inq_varid(ncid, "time", &time_id);
+	
+	status = nc_inq_varid(ncid, varnameA, &Nincel_id);
+	status = nc_inq_varid(ncid, varnameB, &cNincel_id);
+	
+	
+	
+	start[0] = nrec;
+	//pstart[0] = nrec;    
+	tst[0]=nrec;
+
+	//Provide values for variables
+	status = nc_put_var1_float(ncid, time_id,tst,&totaltime);
+	
+	status = nc_put_vara_short(ncid, Nincel_id, start, count, uu);
+	status = nc_put_vara_short(ncid, cNincel_id, start, count, vv);
+	
+	
+
+	//close and save
+	status = nc_close(ncid);
+
+
+}
